19053dfb5ad55f3f68b6ce0828db33eef5dec6b9
liuzihao <liuzihao@me>
2022-01-22 16:07:57 +0800

    提交main

---
+++ src/com/harry/test/DemoMain.java
@@ -0,0 +1,11 @@
+package com.harry.test;
+
+/**
+ * @author liuzihao
+ * @create 2022-01-22-16:07
+ */
+public class DemoMain {
+    public static void main(String[] args) {
+        System.out.println("sources.....main");
+    }
+}
